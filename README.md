2dc5289819091abe087a01bae9881f9de6e9dd3d
Glynn Bird <glynn.bird@gmail.com>
2015-06-01 21:36:13 +0100

    added the recording of state

--- README.md
+++ README.md
@@ -2,11 +2,22 @@
 
 A simple Node.js application to demonstrate how Cloudant's changes feed can be used to write data to local text file as changes occur in the database.
 
-Edit the serialize.js file and change <myaccount> to your personal Cloudant acccount name. The code assumes your database is world readable.
+The Cloudant account name and database name are defined as environment variables e.g.
+
+    export ACCOUNT=myaccount
+    export DB=mydb
+
+Alternatively, this data can be placed in a `.env` file:
+
+    ACCOUNT=myaccount
+    DB=mydb
+
+The code assumes your database is world readable.
   
 * clone repository
 * run 'npm install'
-* edit serialize.js to put your own account name in
+* setup .env or environment variables
 * run 'node serialize.js'
 * add a document via the Cloudant dashboard
 * see a row created in "output.txt" for each document added, edited or deleted
+* state is recorded in `state.json` every minute
--- serialize.js
+++ serialize.js
@@ -1,12 +1,12 @@
-var Cloudant = require('cloudant'),
+require('dotenv').load();
+
+var cloudant = require('cloudant')({account: process.env.ACCOUNT}),
+  mydb = cloudant.db.use(process.env.DB),
   async = require('async'),
-  fs = require('fs');
-  
-var myaccount = "<myaccount>",
-  mydb = "testdb",
+  fs = require('fs'),
+  statefile = "state.json",
   outputfile = "output.txt",
   last_seq = null;
-    
   
 // setup an async queue to process changes one at a time
 var q = async.queue(function(task, callback) {
@@ -14,16 +14,27 @@ var q = async.queue(function(task, callback) {
   fs.appendFile(outputfile, values.join("\t")+"\n", callback);
 }, 1); 
  
-  
+ 
+// save the last_seq every minute
+setInterval(function() {
+  if (last_seq != null) {
+    fs.writeFileSync(statefile, JSON.stringify({ last_seq: last_seq}));    
+  }
+}, 60 * 1000);  
+
 // listen to the changes feed
-Cloudant({account: myaccount}, function(er, cloudant) {
-  // get changes since 'now', and include the document body
-  var feed = cloudant.use(mydb).follow({since:'now', include_docs:true});
-  feed.on('change', function (change) {
-    // add an item to the queue
-    q.push(change.doc);
-    last_seq = change.seq;
-    console.log(last_seq);
-  });
-  feed.follow()
+var options = { since: 'now', include_docs:true };
+try {
+  var state = JSON.parse(fs.readFileSync(statefile));
+  options.since = state.last_seq;  
+} catch (e) {
+}
+console.log("Listening to the", process.env.DB, "_changes feed from", options.since);
+var feed = mydb.follow(options);
+feed.on('change', function (change) {
+  // add an item to the queue
+  q.push(change.doc);
+  last_seq = change.seq;
+  console.log("New change:", last_seq);
 });
+feed.follow()
